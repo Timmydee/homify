c09d643b07477a1bff7ea8badf00fd26b95f8291
Timmydee <dahunsitimmy@gmail.com>
2023-01-23 13:25:28 -0800

    Install ReactJs and taiwind css

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1 className='text-2xl bg-red-500'>HAPPY</h1>
+    </div>
+  )
+}
+
+export default App
