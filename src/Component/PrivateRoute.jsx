24fe6540107ba257579f2daae9dea3a482c8b241
Timmydee <dahunsitimmy@gmail.com>
2023-02-04 20:34:19 -0800

    spinner functionality

--- src/Component/PrivateRoute.jsx
+++ src/Component/PrivateRoute.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Outlet, Navigate} from "react-router-dom"
 import useAuthStatus from '../Hooks/useAuthStatus';
+import Spinner from './Spinner';
 
 //A private route to determine where the site to show
 export default function PrivateRoute() {
     const { loggedIn, checkingStatus } = useAuthStatus();
     if (checkingStatus) {
-      return <h3>Loading.....</h3>;
+      return <Spinner/>;
     }
     return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
   }
